f74bc816ae11b57fde928dcad1e8d4d60fbb2e01
Ty Lewis <tyglewis@gmail.com>
2014-02-14 15:24:31 -0700

    Fixed the graph.

--- backprop_proj/data/vowel_hidden/avg_trials.py
+++ backprop_proj/data/vowel_hidden/avg_trials.py
@@ -13,51 +13,6 @@ def parseCsvData(csvFileName):
 
 
 def plotAccAndMse(csvFileName):
-
-	#fig, ax = plt.subplots()		
-	#axes = [ax, ax.twinx(), ax.twinx()]	
-
-	#fig.subplots_adjust(right=0.75)
-	#axes[-1].spines['right'].set_position(('axes', 1.2))
-
-	#axes[-1].set_frame_on(True)
-	#axes[-1].patch.set_visible(False)
-
-	"""
-	data = parseCsvData(csvFileName)
-	hiddenNodes = [row[0] for row in data]
-	hiddenNodes = hiddenNodes[1:]
-	tsAcc = [row[1] for row in data]
-	tsAcc = tsAcc[1:]
-	tsMse = [row[2] for row in data]
-	tsMse = tsMse[1:]
-	vsAcc = [row[3] for row in data]
-	vsAcc = vsAcc[1:]
-	vsMse = [row[4] for row in data]
-	vsMse = vsMse[1:]
-
-	print hiddenNodes
-	print tsAcc
-	#axes[0].plot(range(len(tsAcc)), tsAcc)
-	#axes[1].plot(range(len(tsAcc)), tsAcc)
-
-	#plt.plot(range(len(tsAcc)), tsAcc)
-	plt.plot(hiddenNodes, tsAcc)
-	plt.xscale("log", basex=2)
-	plt.show()	
-	"""
-
-	"""
-	colors = ("Green", "Red", "Blue")
-	for ax, color in zip(axes, colors):
-		data = np.random.random(1) * np.random.random(10)
-		ax.plot(data, marker='o', linestyle='none', color=color)
-		ax.set_ylabel('%s Thing' % color, color=color)
-		ax.tick_params(axis='y', colors=color)
-	axes[0].set_xlabel('X-axis')
-	plt.show()	
-	"""
-
 	data = parseCsvData(csvFileName)
 
 	learningRate = [row[0] for row in data]
@@ -85,8 +40,7 @@ def plotAccAndMse(csvFileName):
 		tick.set_color("b")
 	yin, yax = plt.ylim()
 	plt.ylim(ymax = yax + .2)
-	#plt.legend(loc=2)
-	plt.title("Accuracy and MSE vs. Learning Rate", fontsize=24)
+	plt.title("Accuracy, MSE, and Training Time vs. Learning Rate", fontsize=18)
 
 	tsMsePlot = tsAccPlot.twinx()
 	tsMsePlot.plot(learningRate, tsMse, "r", linestyle="--", linewidth = 2, label = "MSE for TS")
@@ -96,21 +50,21 @@ def plotAccAndMse(csvFileName):
 	tsMsePlot.set_ylabel("Mean Squared Error (MSE)", color="r", fontsize=18)
 	for tick in tsMsePlot.get_yticklabels():
 		tick.set_color("r")
-	#plt.legend(loc=1)
 
 	epochsPlot = tsAccPlot.twinx()
 	epochsPlot.spines['right'].set_position(('axes', 1.2))
-	l5 = epochsPlot.plot(learningRate, epochs, linestyle="-.", color="y", linewidth = 2, label = "Epochs")
-	epochsPlot.set_ylabel("Epochs to Train", color="g", fontsize=18)
+	l5 = epochsPlot.plot(learningRate, epochs, linestyle="-.", color="#FF6600", linewidth = 2, label = "Epochs to Train")
+	epochsPlot.set_ylabel("Epochs to Train", color="#FF6600", fontsize=18)
 	for tick in epochsPlot.get_yticklabels():
-		tick.set_color("g")
+		tick.set_color("#FF6600")
 	epochsPlot.set_frame_on(True)
 	epochsPlot.patch.set_visible(False)
 	#plt.legend(loc=3)
 	print epochs
 
 	fig.legend((epochsPlot.lines[0], tsAccPlot.lines[0], tsAccPlot.lines[1], tsMsePlot.lines[0], tsMsePlot.lines[1]), 
-	(epochsPlot.lines[0].get_label(), tsAccPlot.lines[0].get_label(), tsAccPlot.lines[1].get_label(), tsMsePlot.lines[0].get_label(), tsMsePlot.lines[1].get_label()), loc = 1)
+	(epochsPlot.lines[0].get_label(), tsAccPlot.lines[0].get_label(), tsAccPlot.lines[1].get_label(), tsMsePlot.lines[0].get_label(), tsMsePlot.lines[1].get_label()), 
+	bbox_to_anchor=(-0.25, -0.1, 1, 1))
 
 	plt.show()
 
